44350ee52517f12c9882245bcab01e41fe623b3e
beecommrnd <beecommrnd@gmail.com>
2025-03-11 17:25:40 +0200

    add morgan for dev mode

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,17 @@ import { appRouter } from './routes';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import compression from 'compression';
+import morgan from 'morgan';
 
 const run = (app: Express) => {
   app.use(compression());
   app.use(cors());
   app.use(bodyParser.json());
+
+  // TODO: check that its not pring logs on production !
+  if (process.env.NODE_ENV === 'DEV') {
+    app.use(morgan('dev'));
+  }
 
   app.use('/api', appRouter);
 
