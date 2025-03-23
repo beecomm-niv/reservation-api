1ff7c26743f25129ec21285b1e5f034a5edc3358
beecommrnd <beecommrnd@gmail.com>
2025-03-23 09:28:32 +0200

    dump full aws-sdk

--- src/database-init.ts
+++ src/database-init.ts
@@ -1,12 +1,5 @@
-import { config } from 'aws-sdk';
 import admin from 'firebase-admin';
 import serviceAccount from '../serviceAccountKey.json';
-
-config.update({
-  region: 'il-central-1',
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-});
 
 admin.initializeApp({
   credential: admin.credential.cert({
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,6 @@
-import { DynamoDB } from 'aws-sdk';
+import { BatchGetCommand, BatchWriteCommand, DynamoDBDocumentClient, GetCommand, PutCommand, QueryCommand, UpdateCommand } from '@aws-sdk/lib-dynamodb';
 import { ErrorResponse } from '../models/error-response.model';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
 interface Expressions<T> {
   expression: keyof T;
@@ -15,10 +16,18 @@ interface InsertOptions<T> {
 export class DB {
   protected static instance: DB;
 
-  public client: DynamoDB.DocumentClient;
+  public client: DynamoDBDocumentClient;
 
   constructor() {
-    this.client = new DynamoDB.DocumentClient();
+    this.client = DynamoDBDocumentClient.from(
+      new DynamoDBClient({
+        region: 'il-central-1',
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY || '',
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+        },
+      })
+    );
   }
 
   public static getInstance = () => {
@@ -35,23 +44,23 @@ export class DB {
     const condition = deniedOverride ? `attribute_not_exists(#${options.primaryKey.toString()})` : undefined;
     const alias = deniedOverride ? { [`#${options.primaryKey.toString()}`]: options?.primaryKey.toString() } : undefined;
 
-    await this.client
-      .put({
+    await this.client.send(
+      new PutCommand({
         TableName: tableName,
         Item: item as any,
         ExpressionAttributeNames: alias,
         ConditionExpression: condition,
       })
-      .promise();
+    );
   };
 
-  public findItemByKey = async <T>(tableName: string, key: DynamoDB.DocumentClient.Key): Promise<T> => {
-    const response = await this.client
-      .get({
+  public findItemByKey = async <T>(tableName: string, key: Record<string, any>): Promise<T> => {
+    const response = await this.client.send(
+      new GetCommand({
         TableName: tableName,
         Key: key,
       })
-      .promise();
+    );
 
     if (!response.Item) {
       throw ErrorResponse.ItemNotFound();
@@ -77,8 +86,8 @@ export class DB {
   public update = async <T>(tableName: string, primaryKey: keyof T, primaryKeyValue: string, expressions: Expressions<T>[]) => {
     const { names, values, expressionString } = this.getNameAndValuesExpressions(expressions, ', ');
 
-    await this.client
-      .update({
+    await this.client.send(
+      new UpdateCommand({
         Key: { [primaryKey]: primaryKeyValue },
         TableName: tableName,
         UpdateExpression: `set ${expressionString}`,
@@ -86,28 +95,28 @@ export class DB {
         ExpressionAttributeValues: values,
         ConditionExpression: `attribute_exists(${primaryKey.toString()})`,
       })
-      .promise();
+    );
   };
 
   public query = async <T>(tableName: string, indexName: string, expressions: Expressions<T>[]) => {
     const { names, expressionString, values } = this.getNameAndValuesExpressions(expressions, ' AND ');
 
-    return await this.client
-      .query({
+    return await this.client.send(
+      new QueryCommand({
         TableName: tableName,
         IndexName: indexName,
         KeyConditionExpression: expressionString,
         ExpressionAttributeNames: names,
         ExpressionAttributeValues: values,
       })
-      .promise();
+    );
   };
 
   public multiWrite = async <T>(tableName: string, items: T[]) => {
     if (!items.length) return;
 
-    await this.client
-      .batchWrite({
+    await this.client.send(
+      new BatchWriteCommand({
         RequestItems: {
           [tableName]: items.map((i) => ({
             PutRequest: {
@@ -116,19 +125,19 @@ export class DB {
           })),
         },
       })
-      .promise();
+    );
   };
 
-  public multiGet = async <T>(tableName: string, keys: DynamoDB.DocumentClient.KeyList) => {
-    const response = await this.client
-      .batchGet({
+  public multiGet = async <T>(tableName: string, keys: Record<string, any>[]) => {
+    const response = await this.client.send(
+      new BatchGetCommand({
         RequestItems: {
           [tableName]: {
             Keys: keys,
           },
         },
       })
-      .promise();
+    );
 
     return (response.Responses?.[tableName] || []) as T[];
   };
